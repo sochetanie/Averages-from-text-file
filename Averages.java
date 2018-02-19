2225836def91b736f3005899f08295cbae5fef28
github username <github email address>
2018-02-19 15:36:44 -0500

    done

---
+++ Averages.java
@@ -0,0 +1,61 @@
+package Lab1_2;
+import java.io.*;
+
+public class Averages {
+  public static void main(String[] args) {
+    String input1;
+    String[] nums ;
+
+    try {
+
+      int countLines = 0;
+      FileReader fileReader = new FileReader(System.getProperty("user.dir")+"/src/Lab1_2/numbers.text");
+      BufferedReader bufferedReader = new BufferedReader(fileReader);
+       while ((input1 = bufferedReader.readLine()) != null){
+            nums = input1.split(" ");
+            double sum=0.0;
+            countLines++;
+            StringBuilder ints = new StringBuilder();
+        for (int i = 1; i < nums.length; i++) {
+          sum+=Integer.parseInt(nums[i]);
+          ints.append(nums[i]).append(" ");
+        }
+        System.out.println(String.format("The average of the %d integers %sis ", nums.length-1, ints.toString()) + sum/(nums.length-1) );
+      }
+      System.out.println(countLines+" sets of numbers processed");
+    } catch ( IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+}
+
+
+
+
+
+
+
+//  The file numbers.text conisits of sequences of numbers, each sequence preceded by a header value nd then followed by that many integers. Read in the sequences and print their averages. When all sequences have been read in, print out the number of sequences processed.
+//
+//
+//
+//    The name of your class should be Averages.
+//
+//    For example, if the file numbers.text contains:
+//
+//    3 1 2 3
+//    5 12 14 6 4 0
+//    10 1 2 3 4 5 6 7 8 9 10
+//    1 17
+//    2 90 80
+//
+//
+//    the program should produce the following output:
+//
+//    The average of the 3 integers 1 2 3 is 2.0
+//    The average of the 5 integers 12 14 6 4 0 is 7.2
+//    The average of the 10 integers 1 2 3 4 5 6 7 8 9 10 is 5.5
+//    The average of the 1 integers 17 is 17.0
+//    The average of the 2 integers 90 80 is 85.0
+//    5 sets of numbers processed
